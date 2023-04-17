5a70a134acedc5c03d050f6101e6cfce174b2718
andylin512 <andymail512@gmail.com>
2023-04-16 23:23:33 -0700

    update path

--- starter/test_model.py
+++ starter/test_model.py
@@ -15,7 +15,7 @@ from starter.ml.data import process_data
 # Create Fixture
 @pytest.fixture(scope="module")
 def path():
-    return "./data/census.csv"
+    return "./starter/data/census.csv"
 
 @pytest.fixture(scope="module")
 def data(path):
